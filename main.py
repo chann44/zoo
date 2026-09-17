0abd6972a85fdb62df3abdb025caac6e831bb9a2
chann44 <44chansw@gmail.com>
2026-09-17 14:55:03 +0530

    simple fast api server

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from zoo!")
+from server.server import Server
 
+
+server = Server()
+app = server.app
+
+def main():
+    
+    server.start(import_string="main:app")
 
 if __name__ == "__main__":
     main()
--- server/server.py
+++ server/server.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+import uvicorn
 
-app = FastAPI()
 
-items = []
-
-class Todo(BaseModel):
-    title: str
+class Server:
+    def __init__(self, port=8000):
+        self.app = FastAPI()
+        self.port = port
     
+    def start(self, import_string: str = "main:app"):
+        app = FastAPI()
+        uvicorn.run(import_string, host="127.0.0.1", port=self.port, reload=True)
+        
+         
+
+   
